b0b5b78cad8760838ba569f2027ce41c670f5fc1
FakhrialAdam <nurachan2@gmail.com>
2020-09-16 12:18:04 +0700

    Biodata

---
+++ Tugas1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	
+	printf(">>>BIODATA<<<\n");
+	printf("Nama		: Fakhrial Adam\n");
+	printf("NPM		: 19081010093\n");
+	printf("Jurusan		: Teknik Informatika\n");
+	printf("Angkatan	: 19\n");
+	printf("Umur		: 20 tahun\n");
+	printf("Pendidikan	: Universitas Pembangunan Nasional 'Veteran' Jawa Timur\n");
+	printf("Minat		: Desain, Komputer, Fotografi, Game");
+	
+	return 0;
+}
